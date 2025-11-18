6090cf97de32172408b1db637898d08fca255923
mathis-elr <mathis.eloire15@gmail.com>
2025-11-18 22:40:39 +0100

    Fonction ajouterInvestissementTotal, tous les test on été passés

--- Investissement_WebClient.Core/Investir.cs
+++ Investissement_WebClient.Core/Investir.cs
@@ -1,7 +1,9 @@
 ﻿using Investissement_WebClient.Data.Modeles;
 using Investissement_WebClient.Data.Repository.Interfaces;
 using System.ComponentModel.Design;
+using System.Diagnostics;
 using System.Reflection.Metadata;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Investissement_WebClient.Core
 {
@@ -33,75 +35,66 @@ namespace Investissement_WebClient.Core
         {
             _Iinvestir.AddInvest(transactions);
 
-            ajouterInvestissementTotal(transactions);
+            this.ajouterInvestissementTotal(transactions);
         }
 
         private void ajouterInvestissementTotal(List<Transaction> transactions)
         {
-            DateTime datePremierInvest = _Iinvestir.getDatePremierInvest();
-            DateTime? dateDernierInvest = _Iinvestir.getDateDernierInvest();
             DateTime dateInvest = transactions[0].date.Date;
             DateTime dateDuJour = DateTime.Today;
-
-            //cas 1 : On investit pour la première fois
-            this.verifierExistanceDernierInvestissement(dateDernierInvest);
-
-            DateTime dateDernierInvestNonNull = Convert.ToDateTime(_Iinvestir.getDateDernierInvest());
-
-            //cas 2 : Investissment dans le futur -> Impossible
+            //cas 1 : Investissment dans le futur -> Impossible
             if (dateInvest > dateDuJour)
             {
                 throw new Exception("La date de la transaction ne peut pas être dans le futur.");
             }
+
+            DateTime? dateDernierInvestNullable = _Iinvestir.getDateDernierInvest();
+            //cas 1 : On investit pour la première fois
+            if (dateDernierInvestNullable == null)
+            {
+                _Iinvestir.ajouterInvestissementTotal(dateInvest.AddDays(-1), 0); //on ajoute au jour precedent le jour de l'investissement
+            }
+
+            //on rajoute les investissements manquants, du dernier invest+1 jusqu'a ajd inclu
+            DateTime dateDernierInvestNonNull = Convert.ToDateTime(_Iinvestir.getDateDernierInvest());
+            this.ajouterInvestissementsTotauxManquant(dateDernierInvestNonNull, dateDuJour);
 
             double montant = this.calculerInvestissementTotal(transactions);
 
             //cas 3: Investissement dans le présent
             if (dateInvest == dateDuJour)
             {
-                //cas 3.1 : le dernier invest n'était pas le jour précedent cet investissement
-                if (dateDernierInvest != dateInvest.AddDays(-1))
-                {
-                    this.ajouterInvestissementsTotauxManquant(dateDernierInvestNonNull, dateInvest);
-                }
-
-                //cas 3.2 : le dernier invest était le jour précedent cet investissement
-                _Iinvestir.ajouterInvestissementTotal(dateInvest, montant);
+                _Iinvestir.modifierInvestissementTotal(dateInvest, montant);
             }
 
-            //cas 4 : On investit dans le passé
-            if(dateInvest < dateDuJour)
-            {
-                this.ajouterInvestissementsTotauxManquant(dateDernierInvestNonNull, dateDuJour);
+            DateTime datePremierInvest = _Iinvestir.getDatePremierInvest();
 
-                //cas 4.1 : la date du premier invest est plus récente que la date d'invest
-                if (datePremierInvest > dateInvest)
+            //cas 4 : On investit dans le passé..
+            if (dateInvest < dateDuJour)
+            {
+                //cas 4.1 : et la date du premier invest est plus récente que la date d'invest
+                if (dateInvest < datePremierInvest)
                 {
-                    _Iinvestir.ajouterInvestissementTotal(dateInvest, montant);
                     this.changerDatePremierInvest(dateInvest);
                     this.changerAncienPremierInvest(datePremierInvest, montant);
-                    this.ajouterInvestissementsTotauxManquant(dateInvest, datePremierInvest);
+
+                    _Iinvestir.ajouterInvestissementTotal(dateInvest, montant);
+                    this.ajouterInvestissementsTotauxManquant(dateInvest, datePremierInvest.AddDays(-1));
                     this.modifierInvestissementsTotauxSuivant(datePremierInvest, montant);
                 }
-                //cas 4.2 : mais la date d'invest est plus récente que le dernier invest
-                else if (dateInvest > dateDernierInvestNonNull)
-                {
-                    _Iinvestir.ajouterInvestissementTotal(dateInvest, montant);
-                }
-                //cas 4.3 : mais la date d'invest est plus lointaine que le denrier invest
-                else if (dateInvest <= dateDernierInvestNonNull)
+                //cas 4.2 : et la date d'invest est plus récente que le dernier invest
+                else if (dateInvest > datePremierInvest)
                 {
                     _Iinvestir.modifierInvestissementTotal(dateInvest, montant);
                     this.modifierInvestissementsTotauxSuivant(dateInvest, montant);
                 }
-            }
-        }
-
-        private void verifierExistanceDernierInvestissement(DateTime? date)
-        {
-            if (date == null)
-            {
-                _Iinvestir.ajouterInvestissementTotal(DateTime.Today.AddDays(-1), 0); //on ajoute au jour precedent ajd
+                ////cas 4.3 : et la date d'investissement correspond au premier investissement (celui qui vaut 0)
+                else if (dateInvest == datePremierInvest)
+                {
+                    this.changerDatePremierInvest(dateInvest);
+                    _Iinvestir.modifierInvestissementTotal(dateInvest, montant);
+                    this.modifierInvestissementsTotauxSuivant(dateInvest, montant);
+                }
             }
         }
 
@@ -112,14 +105,12 @@ namespace Investissement_WebClient.Core
 
         private void ajouterInvestissementsTotauxManquant(DateTime dateDebut, DateTime dateFin)
         {
-            DateTime dateCourante = Convert.ToDateTime(dateDebut).AddDays(1); //+1j car on a dejà la date du dernierInvest
-            if (dateCourante != dateFin)
+            DateTime dateCourante = Convert.ToDateTime(dateDebut).AddDays(1);
+
+            while (dateCourante <= dateFin)
             {
-                while (dateCourante < dateFin)
-                {
-                    _Iinvestir.ajouterInvestissementTotal(dateCourante, 0);
-                    dateCourante = dateCourante.AddDays(1);
-                }
+                _Iinvestir.ajouterInvestissementTotal(dateCourante, 0);
+                dateCourante = dateCourante.AddDays(1);
             }
         }
 
@@ -139,8 +130,9 @@ namespace Investissement_WebClient.Core
             DateTime dateDernierInvest = Convert.ToDateTime(_Iinvestir.getDateDernierInvest());
             DateTime dateCourante = datePremierInvest.AddDays(1);
 
-            while (dateCourante < dateDernierInvest)
+            while (dateCourante <= dateDernierInvest)
             {
+                Console.WriteLine(dateCourante);
                 _Iinvestir.modifierInvestissementTotal(dateCourante, montant);
                 dateCourante = dateCourante.AddDays(1);
             }
