6899f7ef1cb2ffd81d6b3ee17ea85fbe0ee35dbb
mathis-elr <meloire@univ-pau.fr>
2026-02-09 21:15:31 +0100

    Mise au propre du code front concernant Actif, avec un code qui respect DRY

--- Investissement_WebClient.Core/Modeles/Actif.cs
+++ Investissement_WebClient.Core/Modeles/Actif.cs
@@ -2,19 +2,23 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Investissement_WebClient.Core.Modeles;
 
-public class Actif
+public class Actif : IActif
 {
     public int Id { get; set; }
     
+    [Required(ErrorMessage = "Nom requis.")]
     public string Nom { get; set; } 
     
-    public ActifType Type { get; set; } 
+    [Required(ErrorMessage = "Type requis.")]
+    public ActifType? Type { get; set; } 
     
     [StringLength(12, MinimumLength = 12, ErrorMessage = "L'ISIN doit faire exactement 12 caractères.")]
-    [RegularExpression(@"^[A-Z]{2}[A-Z0-9]{9}[0-9]$", ErrorMessage = "Format ISIN invalide.")]
+    [RegularExpression(@"^[A-Z]{2}[A-Z0-9]{9}[0-9]$", ErrorMessage = "Format de l'ISIN invalide.")]
     public string? Isin { get; set; } 
     
+    [Required(ErrorMessage = "Symbole requis.")]
     public string Symbole { get; set; }
     
-    public ActifRisque Risque { get; set; }
+    [Required(ErrorMessage = "Niveau de risque requis.")]
+    public ActifRisque? Risque { get; set; }
 }
--- Investissement_WebClient.Core/Modeles/ActifEnregistre.cs
+++ Investissement_WebClient.Core/Modeles/ActifEnregistre.cs
@@ -2,21 +2,25 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Investissement_WebClient.Core.Modeles;
 
-public class ActifEnregistre
+public class ActifEnregistre : IActif
 {
     public int Id { get; set; }
     
+    [Required(ErrorMessage = "Nom requis.")]
     public string Nom { get; set; }
     
+    [Required(ErrorMessage = "Symbole requis.")]
     public string Symbole { get; set; }
     
-    public ActifType Type { get; set; }
+    [Required(ErrorMessage = "Type requis.")]
+    public ActifType? Type { get; set; }
     
     [StringLength(12, MinimumLength = 12, ErrorMessage = "L'ISIN doit faire exactement 12 caractères.")]
     [RegularExpression(@"^[A-Z]{2}[A-Z0-9]{9}[0-9]$", ErrorMessage = "Format ISIN invalide.")]
     public string? Isin { get; set; }
     
-    public ActifRisque Risque { get; set; }
+    [Required(ErrorMessage = "Niveau de risque requis.")]
+    public ActifRisque? Risque { get; set; }
     
     public ICollection<Transaction>? Transactions { get; set; }
     
