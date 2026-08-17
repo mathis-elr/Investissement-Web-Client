a8f2d61643045ab49d183aa85f91ac57181b1868
mathis-elr <meloire@univ-pau.fr>
2026-08-17 11:42:09 +0200

    Nav bar beaucoup plus prenium et plus sympa à utiliser (slider qui bouge)

---
+++ Investissement_WebClient.Web/Components/Layout/MainLayout.razor.js
@@ -0,0 +1,11 @@
+﻿export function moveSlider() {
+
+    const link = document.querySelector(".nav-link.active");
+    const slider = document.querySelector(".slider");
+
+    if (!link || !slider)
+        return;
+
+    slider.style.width = `${link.offsetWidth}px`;
+    slider.style.transform = `translateX(${link.offsetLeft - 5}px)`;
+}
